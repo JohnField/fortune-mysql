c28a517fcc8c0832b09ab4c36ee75a31cbae7246
John Field <John.Field@gMail.com>
2017-12-30 23:51:17 +0000

    Linting (inc. pre-existing?)

--- test/index.js
+++ test/index.js
@@ -5,7 +5,6 @@ testAdapter(adapter, {
   url: 'mysql://mysql@localhost:3306/fortune_test',
   primaryKeyType: 'integer',
   useForeignKeys: true,
-  // isNative: true,
   generatePrimaryKey: () => Math.floor(Math.random() * Math.pow(2, 16)),
   typeMap: {
     user: 'users',
